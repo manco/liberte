16b8854e58eaf1790741a486c9f1d759177ab480
Krzysztof Mozejko <k.mozejko@ocado.com>
2025-11-05 22:58:09 +0100

    actual Parser class with logic

--- liberte/parser.py
+++ liberte/parser.py
@@ -1,7 +1,38 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from bs4.element import AttributeValueList
+from pymonad.maybe import Maybe
+from pymonad.maybe import Nothing
+from pymonad.maybe import Just
+import re
 
-def has_new_messages(input_html):
-   return True
+from typing import Optional, TypeVar
 
-def has_new_announcements(input_html):
-   return True
+class Parser:
+   def __init__(self, input_html: str):
+      self.soup = BeautifulSoup(input_html, 'html.parser')
+      self.messages_title_pattern: re.Pattern[str] = re.compile("^Liczba nieprzeczytanych wiadomości:\\s*\\d+$")
+      self.announcements_title_pattern: re.Pattern[str] = re.compile("^Liczba nieprzeczytanych ogłoszeń:\\s*\\d+$")
+
+   def has_new_messages(self) -> bool:
+      return self.__find_anchor_match_title('icon-wiadomosci', self.messages_title_pattern)
+
+   def has_new_announcements(self) -> bool:
+      return self.__find_anchor_match_title('icon-ogloszenia', self.announcements_title_pattern)
+
+   def __find_anchor_match_title(self, anchor_id: str, title_regex: re.Pattern[str]) -> bool:
+      return (
+         Parser
+         .__option(self.soup.find('a', id=anchor_id))
+         .bind(lambda a:Parser.__option(a.get('title'))) # type: ignore[arg-type] pylance is stoopid
+         .bind(lambda t:Parser.__option(title_regex.search(t))) # type: ignore[arg-type] pylance is stoopid
+         .is_just()
+      )
+
+   T = TypeVar('T')
+   @staticmethod
+   def __option(x: Optional[T]) -> Maybe[T]:
+      if not x:
+         return Nothing
+      else:
+         return Just(x)
+
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,26 +8,32 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-import liberte.parser
+from liberte.parser import Parser
 
 def test_finds_new_messages():
     HTML = '''<a title="Liczba nieprzeczytanych wiadomości: 2" href="/wiadomosci" id="icon-wiadomosci"><span class="circle"></span>Wiadomości<a class="button counter">2         </a></a>                    '''
-    assert liberte.parser.has_new_messages(HTML) == True
+    assert Parser(HTML).has_new_messages() == True
 
-# def test_not_find_new_messages():
-    # assert liberte.parser.has_new_messages('') == False  
+def test_not_find_new_messages():
+    assert Parser('').has_new_messages() == False
 
 def test_finds_new_announcements():
     HTML = '''<a title="Liczba nieprzeczytanych ogłoszeń: 1" href="/ogloszenia" id="icon-ogloszenia"><span class="circle"></span>Ogłoszenia</a>                    <a class="button counter">1         </a>'''
-    assert liberte.parser.has_new_messages('') == True           
+    assert Parser(HTML).has_new_announcements() == True
 
-# def test_not_find_new_announcements():
-    # assert liberte.parser.has_new_messages('') == False  
+def test_not_find_new_announcements():
+    assert Parser('').has_new_messages() == False
 
 def test_find_new_announcements_and_messages_in_real_html():
     with open('tests/site-with-notifications.html', 'r') as file:
-        input_html = file.read()   
-        assert liberte.parser.has_new_messages(input_html) == True
-        assert liberte.parser.has_new_announcements(input_html) == True  
+        parser = Parser(file.read())
+        assert parser.has_new_messages() == True
+        assert parser.has_new_announcements() == True
 
+
+def test_not_find_new_announcements_and_messages_in_real_html():
+    with open('tests/site-no-notifications.html', 'r') as file:
+        parser = Parser(file.read())
+        assert parser.has_new_messages() == False
+        assert parser.has_new_announcements() == False
  
